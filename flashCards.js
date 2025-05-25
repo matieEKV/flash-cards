bb4fd7cab9ed8ffebdf380eb8cb89d3d0b0ee21f
MKanunnikau <matea.ekv@gmail.com>
2025-05-25 22:50:38 +0200

    added hide button functionality

--- flashCards.js
+++ flashCards.js
@@ -11,6 +11,7 @@ let modalBtn = document.getElementById('createPlayer');
 let modalContainer = document.getElementById('newPlayersModal');
 let closeBtn = document.getElementById('cancelButton');
 
+
 // Open popup window for download cards and dim the background when button clicked
 function openPopup() {
     if (popup) {    //added a condition to check if popup available, not available in modal
@@ -35,11 +36,12 @@ modalBtn.addEventListener('click', function() {
     document.getElementById("overlay").style.display = "block";
 });
 
-// Close modal window and remove dimmed background
-closeBtn.addEventListener('click', function() {
+function closeModal() {
     modalContainer.classList.remove("open-popup")
     document.getElementById("overlay").style.display = "none";
-});
+}
+// Close modal window and remove dimmed background
+closeBtn.addEventListener('click', closeModal);
 
 // save player names
 let playerOneNameInput = document.getElementById('playerOne');
@@ -47,11 +49,21 @@ let playerTwoNameInput = document.getElementById('playerTwo');
 let startGame = document.getElementById('startGame');
 let playerOneName = document.getElementById('playerOneName');
 let playerTwoName = document.getElementById('playerTwoName');
+let gameButtonContainer = document.querySelector('.gameButtonContainer');
 
 //function that adds user input names to the name element
 function addNames() {
     playerOneName.innerText = playerOneNameInput.value.toUpperCase();
     playerTwoName.innerText = playerTwoNameInput.value.toUpperCase();
 }
-
-startGame.addEventListener('click', addNames);
+//function that hides the "Add new players" button
+function hideContainer() {
+    gameButtonContainer.style.display = 'none';
+}
+//function that calls threefunction when start button clicked
+function addNamesHideContainer() {
+    addNames();
+    hideContainer();
+    closeModal();
+}
+startGame.addEventListener('click', addNamesHideContainer);
