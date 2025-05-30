a2f660f242c3e2c481bd7f4cda9579393704f9d7
Angela Rakic <andjela.nikolic.arh@gmail.com>
2025-05-30 17:04:38 +0200

    changed small mistakes in array, as chatGPT suggested

--- flashCards.js
+++ flashCards.js
@@ -79,46 +79,48 @@ function addNamesHideContainer() {
 }
 startGame.addEventListener('click', addNamesHideContainer);
 
+//array of cards
+
 let flashCardsArray = [
     {
-        image: "images\hygge.png" ,
+        image: "images/hygge.png" ,
         question: "What is the defining characteristic of Danish culture?",
         answer: "Hygge."
     },
     {
-        image: "images\Lego.png" ,
+        image: "images/Lego.png" ,
         question: "Which world-famous toy is made in Denmark?",
         answer: "Lego"
     },
-    {   image: "images\BIG.png" ,
+    {   image: "images/BIG.png" ,
         question: "What is the name of the world famous contemporary architect from Denmark?",
         answer: "BIG (Bjarke Ingels)"
     },
-    {   image: "images\dannebrog.png" ,
+    {   image: "images/dannebrog.png" ,
         question: "Danes are very proud of their flag. Do you know how they call it?",
         answer: "Dannebrog"
     },
-    {   image: "images\MadsMikkelsen.png" ,
+    {   image: "images/MadsMikkelsen.png" ,
         question: "The famous actor from Denmark, who was nominated for Oscar is..." ,
         answer: "Mads Mikkelsen"
     },
-    {   image: "images\Pandora.png" ,
+    {   image: "images/Pandora.png" ,
         question: "The world-known jewelry brand originated in Copenhagen is..." ,
         answer: "Pandora"
     },
-    {   image:  "images\B&O.png" , 
+    {   image:  "images/B&O.png" , 
         question: "Danish high-end audio and multimedia company known for its quality audio products is called..." ,
         answer: "Bang&Olufsen (B&O)"
     },
-    {   image: "images\ArneJakobsen.png" ,
+    {   image: "images/ArneJakobsen.png" ,
         question: "The name of the Danish architect and furniture designer, known for simple and well-designed chairs is..." ,
         answer: "Arne Jakobsen"
     },
-    {   image: "images\HCAndersen.png", 
+    {   image: "images/HCAndersen.png", 
         question: "Name of the Danish writer of plays, best remembered for his fairy tales is.." ,
         answer: "Hans Christian Andersen"
     },
-    {   image: "images\Oresund.png" ,
+    {   image: "images/Oresund.png" ,
         question: "Name of the second longest bridge in Europe, connecting Denmark and Sweden is..." ,
         answer: "Öresund Bridge"
     }
@@ -141,7 +143,7 @@ let flashCardsArray = [
 
       // Saving user's input as a new const, along with provided default image
       const newCard = {
-        image: "images/defaultImageForNewCard",
+        image: "images/defaultImageForNewCard.png",
         question: questionInput,
         answer: answerInput
       };
@@ -169,11 +171,6 @@ let flashCardsArray = [
     });
 
     // ANDJELA LINES START
-
-
-
-
-
 
 
 
