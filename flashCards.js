f0e2d22abbf83f6a916910dc50b9ba0e4ed75318
Angela Rakic <andjela.nikolic.arh@gmail.com>
2025-06-02 22:37:25 +0200

    comit changes in js

--- flashCards.js
+++ flashCards.js
@@ -1,8 +1,60 @@
 //JavaScript file for FlashCards
 
-//   array
+//   array of cards
+let flashCardsArray = [
+    {
+        image: "images/hygge.png",
+        question: "What is the defining characteristic of Danish culture?",
+        answer: "Hygge."
+    },
+    {
+        image: "images/Lego.png",
+        question: "Which world-famous toy is made in Denmark?",
+        answer: "Lego"
+    },
+    {
+        image: "images/BIG.png",
+        question: "What is the name of the world famous contemporary architect from Denmark?",
+        answer: "BIG (Bjarke Ingels)"
+    },
+    {
+        image: "images/dannebrog.png",
+        question: "Danes are very proud of their flag. Do you know how they call it?",
+        answer: "Dannebrog"
+    },
+    {
+        image: "images/MadsMikkelsen.png",
+        question: "The famous actor from Denmark, who was nominated for an Oscar, is...",
+        answer: "Mads Mikkelsen"
+    },
+    {
+        image: "images/Pandora.png",
+        question: "The world-known jewelry brand originated in Copenhagen is...",
+        answer: "Pandora"
+    },
+    {
+        image: "images/B&O.png",
+        question: "Danish high-end audio and multimedia company known for its quality audio products is called...",
+        answer: "Bang & Olufsen (B&O)"
+    },
+    {
+        image: "images/ArneJakobsen.png",
+        question: "The name of the Danish architect and furniture designer, known for simple and well-designed chairs is...",
+        answer: "Arne Jakobsen"
+    },
+    {
+        image: "images/HCAndersen.png",
+        question: "Name of the Danish writer of plays, best remembered for his fairy tales is...",
+        answer: "Hans Christian Andersen"
+    },
+    {
+        image: "images/Oresund.png",
+        question: "Name of the second longest bridge in Europe, connecting Denmark and Sweden is...",
+        answer: "Öresund Bridge"
+    }
+  ];
 
-
+/*
 //  Start Button Funtionality-Takes you to first question-hygge
 // document.getElementById("startButton") .onclick = function () {
 //     window.location.href = "startLearningpage.html";
@@ -88,48 +140,53 @@ function addNamesHideContainer() {
 if (startGame) {
     startGame.addEventListener('click', addNamesHideContainer);
 }
-
-
-
-    // save the new card from user
-
-    // document.getElementById("submitButton").addEventListener("click", () => {
-    //   const questionInput = document.getElementById("questionFromUser").value.trim();
-    //   const answerInput = document.getElementById("answerFromUser").value.trim();
-
-      // Make sure that user did not submit empty fields
+*/
 
     // ANDJELA LINES START
-    //   if (!questionInput || !answerInput) {
-        // alert("Please enter both a question and an answer");
-    //     return;
-    //   }
+   
+    // Let's check if the array is there
+    console.log(flashCardsArray);
 
-    //   // Saving user's input as a new const, along with provided default image
-    //   const newCard = {
-    //     image: "images/defaultImageForNewCard.png",
-    //     question: questionInput,
-    //     answer: answerInput
-    //   };
+    // save the new card from user
+    document.getElementById("submitButton").addEventListener("click", () => {
+      const questionInputNewCard = document.getElementById("questionFromUser").value;
+      const answerInputNewCard = document.getElementById("answerFromUser").value;
 
-    //   //adding new card on the end of our flashCardArray
-    //   flashCardsArray.push(newCard);
+      
+      // Make sure that user did not submit empty fields
+      if (!questionInputNewCard || !answerInputNewCard) {
+        alert("Please enter both a question and an answer.");
+        return;
+      };
+      
+      // Saving user's input as a new const, along with provided default image
+      const newCard = {
+        image: "images/defaultImageForNewCard",
+        question: questionInputNewCard,
+        answer: answerInputNewCard
+      };
 
-    //   //saving the whole array with the new card on local storage
-    //   //The array needs to be stringified before saving in local storage.
-    //   localStorage.setItem("flashCardsArray", JSON.stringify(flashCardsArray));
+      //adding new card on the end of our flashCardArray
+      flashCardsArray.push(newCard);
 
-    //   //test to see if the card is saved
-    //   console.log("New flashcard saved:", newCard);
-    //   // alert for user > I should style this
-    //   alert("New flashcard saved successfully");
+      //saving the whole array with the new card on local storage
+      //The array needs to be stringified before saving in local storage.
+      localStorage.setItem("flashCardsArray", JSON.stringify(flashCardsArray));
 
-    //   // Clear input fields after saving
-    //   document.getElementById("questionFromUser").value = "";
-    //   document.getElementById("answerFromUser").value = "";
+      //test to see if the card is saved
+      console.log("New flashcard saved:", newCard);
 
-    //   //test to see the array
-    //   console.log("New array is saved in LOCAL STORAGE:", flashCardsArray);
+      // alert for user > I should style this
+      alert("New flashcard saved successfully.");
+
+      // Clear input fields after saving
+      document.getElementById("questionFromUser").value = "";
+      document.getElementById("answerFromUser").value = "";
+
+      //test to see the array
+      console.log("New array is saved in LOCAL STORAGE:", flashCardsArray);
+    });
+
 
 
 
@@ -319,7 +376,7 @@ if (startGame) {
 
 
 
-//  MATEA LINES START
+/*//  MATEA LINES START
 
 //downloading flashcards as json
 
@@ -515,4 +572,5 @@ function downloadJSON() {
 
 
 
-// MATEA LINES STOP
+// MATEA LINES STOp
+*/
